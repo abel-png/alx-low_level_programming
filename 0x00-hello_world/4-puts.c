c2259e3112f4a8f0ae8c17445efbceb3161dab6f
abel-png <abelteshome01@gmail.com>
2022-12-09 12:29:41 -0800

    programming using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - print aline of code using puts
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
+
+}
